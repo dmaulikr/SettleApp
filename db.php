3e17bf63bb6fa05a798794d65d35b5a2ee852324
EResman <elias.resman@gmail.com>
2014-09-18 11:30:42 +0200

    Add username etc

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php 
-$username = "";
-$database = "";
+$username = "109458_wc46452";
+$database = "109458-settleapp";
 
 mysql_connect(settleapp-109458.mysql.binero.se, $username);
 @mysql_select_db($database) or die("Unable to find database");
